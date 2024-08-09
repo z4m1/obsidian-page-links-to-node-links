9f4546f6ea759c2cf79f71fca11d56288ea1db61
z4m1 <syureisaan@gmail.com>
2024-08-09 15:11:05 +0900

    現在のファイルがキャンバスかどうか判定する。

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 
 
-// 不必要な箇所の消去と整形
+// 現在のファイルがキャンバスかどうか判定する。
 
 import {
 	App, 
@@ -18,7 +18,12 @@ export default class MyPlugin extends Plugin {
 		// リボン（左サイドメニュー）にアイコンを追加する。
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
 			// ここに動作を記述。
-			new Notice('This is a notice!');
+			if (this.is_active_leaf_canvas()) {
+				new Notice('True');
+			}
+			else {
+				new Notice('False');
+			}
 		});
 		// HTMLのクラスを追加する。（多分外部から参照する際に必要になる？）
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
@@ -27,4 +32,6 @@ export default class MyPlugin extends Plugin {
 	onunload() {
 
 	}
+
+	is_active_leaf_canvas = () =>  this.app.workspace.activeLeaf?.view.getViewType() === "canvas"
 }
