d7cafa35bd47d68328d5fa2f7681cb7c11477a70
z4m1 <syureisaan@gmail.com>
2024-08-09 16:23:25 +0900

    選択されたノードの情報を取得する。

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 
 
-// 現在のファイルがキャンバスかどうか判定する。
+// 選択されたノードの情報を取得する。
 
 import {
 	App, 
@@ -18,8 +18,21 @@ export default class MyPlugin extends Plugin {
 		// リボン（左サイドメニュー）にアイコンを追加する。
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
 			// ここに動作を記述。
-			if (this.is_active_leaf_canvas()) {
-				new Notice('True');
+			if(this.is_active_leaf_canvas()) {
+				// @ts-ignore
+				const canvas = this.app.workspace.activeLeaf.view.canvas
+				const all_data = canvas.getData();
+				const selected_data = canvas.getSelectionData();
+				
+				// @ts-ignore
+				const selected_file_nodes = Array.from(selected_data.nodes).filter((value) => value.type === "file");
+
+				console.log(all_data);
+				console.log(selected_data);
+				console.log(selected_file_nodes)
+				new Notice("選択されたファイルノードは"+selected_file_nodes.length+"個です。");
+
+				if(selected_file_nodes.length <= 0) return;
 			}
 			else {
 				new Notice('False');
