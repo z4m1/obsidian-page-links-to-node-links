648521c3a3daa1e73d79cd41b160860d23c29f9d
z4m1 <syureisaan@gmail.com>
2024-08-09 14:08:35 +0900

    不必要な箇所の消去と整形

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 
 
+// 不必要な箇所の消去と整形
+
 import {
 	App, 
 	Editor, 
@@ -11,36 +13,15 @@ import {
 	Setting,
 } from 'obsidian';
 
-
-// 不必要な箇所の消去と書式の整頓
 export default class MyPlugin extends Plugin {
 	async onload() {
-		// This creates an icon in the left ribbon.
+		// リボン（左サイドメニュー）にアイコンを追加する。
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
+			// ここに動作を記述。
 			new Notice('This is a notice!');
 		});
-		// Perform additional things with the ribbon
+		// HTMLのクラスを追加する。（多分外部から参照する際に必要になる？）
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
